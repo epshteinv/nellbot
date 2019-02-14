362367e904ee5a846ed834d923132a07c138493f
epshteinv <47326538+epshteinv@users.noreply.github.com>
2019-02-14 13:53:08 +0200

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -35,7 +35,7 @@ def start(bot, update):
     reply_keyboard = [['Help', 'Information', 'Reports']]
 
     update.message.reply_text(
-        'Hi! My name is Nelly, i am EPM Chat Bot. i`d like to help. '
+        'Hi! My name is NellBot, i am EPM Chat Bot. i`d like to help. '
         'Send /cancel to stop talking to me.\n\n'
         'Please select one of these options?',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
